42c571f5db0c4117ad52b0c79b685f13df426ec6
rajutamanglama63 <rajutamanglama63@gmail.com>
2026-07-02 12:34:35 +0545

    add: database connectivity

---
+++ database/src/test/java/com/jarudev/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarudev.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
