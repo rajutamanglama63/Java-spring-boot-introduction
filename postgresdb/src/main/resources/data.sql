ba4fa9b293c9b26ed6f3bd3d4c8ae0d75dbba21a
rajutamanglama63 <rajutamanglama63@gmail.com>
2026-07-02 18:07:24 +0545

    add: postgresql db connection

---
+++ postgresdb/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO widgets (id, name, purpose) VALUES
+(1, 'widget A', 'Used for testing purposes.'),
+(2, 'widget B', 'Designed for entertainment.'),
+(3, 'widget C', 'Enhances productivity.'),
+(4, 'widget D', 'Perfect for outdoor activity.'),
+(5, 'widget A', 'Improve overall well-being.');
---
+++ postgresdb/src/test/java/com/jarudev/postgresdb/PostgresdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarudev.postgresdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
