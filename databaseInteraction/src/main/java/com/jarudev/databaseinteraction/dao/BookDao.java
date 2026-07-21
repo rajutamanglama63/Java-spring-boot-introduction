fafe291395599b84cd51298b40b22af094b1dc82
rajutamanglama63 <rajutamanglama63@gmail.com>
2026-07-21 23:12:34 +0545

    add: find method tested

--- databaseInteraction/src/main/java/com/jarudev/databaseinteraction/dao/BookDao.java
+++ databaseInteraction/src/main/java/com/jarudev/databaseinteraction/dao/BookDao.java
@@ -1,4 +1,12 @@
 package com.jarudev.databaseinteraction.dao;
 
+import com.jarudev.databaseinteraction.domain.Book;
+
+import java.util.Optional;
+
 public interface BookDao {
+
+    void create(Book book);
+
+    Optional<Book> find(String isbn);
 }
